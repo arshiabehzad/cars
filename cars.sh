ee03cbe6ce7592ccc9f7c6942c21156a20a4bb7c
Arshia Behzad <behzad@chapman.edu>
2018-12-05 11:07:26 -0800

    Final version

--- cars.sh
+++ cars.sh
@@ -8,7 +8,8 @@ do
 	echo "1 - add car, 2 - List cars, 0 -quit"
 	read choice
 
-	case $choice in "1")
+	case $choice in 
+	"1")
 		echo "What is the year of the car?"
 		read carYear
 		echo "Who makes the car?"
@@ -16,16 +17,14 @@ do
 		echo "What is the model of the car?"
 		read carModel
 		carName=$carYear":"$carMake":"$carModel
-		echo $carName >> "My_old_cars.txt";;	
-	
-        case $choice in "2")
-		while read -r line
-		do
-			name="$line"
-		done < "My_old_cars.txt";;
-		
-	# case $choice in "0") 
+		echo $carName >> "My_old_cars.txt"	
+		;;
+        "2")
+		sort -n "My_old_cars.txt"
+		;;
+	"0") 
+		stay=false
 		
 	esac 
-	stay=false
 done 
+echo "Good bye"
